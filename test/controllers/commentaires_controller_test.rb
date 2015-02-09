e41e6033a352b5f82c33cbf17f366b72dffa844a
Karb0 <carbonel.mathieu@gmail.com>
2015-02-09 20:10:24 +0100

    Détaille les message d'erreur des tests
    sur le controlleur commentaire

--- test/controllers/commentaires_controller_test.rb
+++ test/controllers/commentaires_controller_test.rb
@@ -28,12 +28,12 @@ class CommentairesControllerTest < ActionController::TestCase
     sign_in contributeurs(:contrib)
     count = Commentaire.count
     post :create, projet_id: 1, commentaire: {message: 'yoo'}
-    assert_not_nil assigns(:commentaire)
-    assert_equal 'yoo', assigns(:commentaire).message
-    assert_equal count + 1, Commentaire.count
-    assert_redirected_to projet_path 1
-    assert_equal 1, assigns(:commentaire).projet_id
-    assert_equal 1, assigns(:commentaire).contributeur_id
+    assert_not_nil assigns(:commentaire), "@commentaire doit être définis"
+    assert_equal 'yoo', assigns(:commentaire).message, "Le commentaire ne contient pas de message"
+    assert_equal count + 1, Commentaire.count, "Le Commentaire doit être sauvegardé dans la base"
+    assert_equal 1, assigns(:commentaire).projet_id, "Le commentaire n'a pas été asocié au bon projet"
+    assert_equal 1, assigns(:commentaire).contributeur_id, "Le commentaire n'a pas été associé au bon contributeur"
+    assert_redirected_to projet_path(1), "L'utilisateur doit être redirigé vers `/projets/1`"
   end
 
 end

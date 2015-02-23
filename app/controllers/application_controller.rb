f46fe400b362e8170b557c32a770f21d9c6e55b4
emak <emilien.ak@gmail.com>
2015-02-23 21:40:56 +0100

    Intégration de la signature de charte
    et ajout du lien vers la page de la charte
    dans le formulaire d'inscription

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   def render_403
     render :status => :forbidden, :text => "Forbidden fruit"
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:pseudo, :nom, :prenom, :annee_naissance, :commune, :charte]
   end
 end

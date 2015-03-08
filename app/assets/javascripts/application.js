e92ef2ef322133db91f089569c97c5d40401e240
emak <emilien.ak@gmail.com>
2015-03-08 23:39:42 +0100

    Refactoring - charge le js de bootstrap depuis les assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require wysihtml-toolbar
 //= require parser_rules/simple
 //= require_tree .

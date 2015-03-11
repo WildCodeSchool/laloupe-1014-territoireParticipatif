c5a158abe02e109f897340e331c28c15725760ed
emak <emilien.ak@gmail.com>
2015-03-11 01:56:21 +0100

    Inscrit un contributeur à la newsletter
    dès le formulaire d'inscription

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310150559) do
+ActiveRecord::Schema.define(version: 20150311001606) do
 
   create_table "actualites", force: :cascade do |t|
     t.string   "titre"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150310150559) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean  "newsletter",             default: false
   end
 
   add_index "contributeurs", ["confirmation_token"], name: "index_contributeurs_on_confirmation_token", unique: true

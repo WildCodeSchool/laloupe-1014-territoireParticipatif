b613daf133dd58ac84ededea21bd10d180a7966c
Karb0 <carbonel.mathieu@gmail.com>
2015-03-20 13:38:26 +0100

    affichage CP sur show index lien urlsite renvoi vers site

--- db/schema.rb
+++ db/schema.rb
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 20150316095017) do
     t.string   "localisation"
     t.date     "demarrage"
     t.string   "urlsite"
-    t.string   "image"
+    t.string   "imageprojet"
   end
 
   add_index "projets", ["categorie_id"], name: "index_projets_on_categorie_id"

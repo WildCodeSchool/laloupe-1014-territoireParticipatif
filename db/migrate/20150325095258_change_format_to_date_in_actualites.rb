9bf0072645db52a4aee25de62ea03991ad081b7d
emak <emilien.ak@gmail.com>
2015-03-25 12:55:30 +0100

    Fix - migration for PG

--- db/migrate/20150325095258_change_format_to_date_in_actualites.rb
+++ db/migrate/20150325095258_change_format_to_date_in_actualites.rb
@@ -1,5 +1,10 @@
 class ChangeFormatToDateInActualites < ActiveRecord::Migration
-  def change
-    change_column :actualites, :date, :date
+  def up
+    remove_column :actualites, :date
+    add_column :actualites, :date, :date
+  end
+  def down
+    remove_column :actualites, :date
+    add_column :actualites, :date, :string
   end
 end
